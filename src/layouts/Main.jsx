6567f1612709cd0a493388f78d0fd7f698d27a5b
eugene0111 <eugenesam1105@gmail.com>
2024-07-02 23:28:57 +0530

    Footer added

--- src/layouts/Main.jsx
+++ src/layouts/Main.jsx
@@ -1,9 +1,11 @@
 import { Outlet } from "react-router-dom";
+import Footer from "../components/Footer/Footer";
 
 export default function Main() {
   return (
     <>
       <Outlet />
+      <Footer />
     </>
   );
 }
